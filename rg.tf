b4c3a8eb1d77011f868d544bfbddec7681cbdbf6
deeep14 <105141185+deeep14@users.noreply.github.com>
2023-05-08 14:59:10 +0530

    Update rg.tf

--- rg.tf
+++ rg.tf
@@ -9,13 +9,14 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "${ARM_SUBSCRIPTION_ID}"
-  client_id = "${ARM_CLIENT_ID}"
-  client_secret = "${ARM_CLIENT_SECRET}"
-  tenant_id = "${ARM_TENANT_ID}"
+  subscription_id = "a60deb51-22a6-4a0b-9d1d-14bc4927a3ee"
+  client_id = "1004fae0-f511-43b5-a8c8-145498a6e4ae"
+  client_secret = "G2t8Q~T6tszAUgPjJmIAC7blHmVWnAeHFbrWnaRP"
+  tenant_id = "51979232-fc51-4638-9cd7-d0c8e1e8eb52"
 }
 
 resource "azurerm_resource_group" "day2-rg" {
     name = "jk-rg101"
     location = "North Europe"
 }
+
